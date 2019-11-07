d9386e03fe7ab306aab82666870de4be8e18a19d
Andressa Tonietto Reis <andressa.reis@solvs.corp>
2019-11-07 17:12:39 -0300

    wip

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,56 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
+  /*initial state can either be set int the constructor
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  or you can use a property: */
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // In order to update state you need to there existing setState Api otherwise is not going to work
+    // 1. Take a copy of the existing state
+    // because you never wnat to reach into state and actually motify it directly
+    const fishes = { ...this.state.fishes };
+
+    // 2. Add our new fish to that fishes available
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3. Set the new fishes object to state
+    this.setState({
+      //you pass it the peace of the state you wish to update
+      fishes: fishes
+      // fishes //if the property and the value are the same thing you can write just one time
+    });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" age={500} cool={true} />
+          <ul className="fishes">{Object.keys(this.state.fishes)}</ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,52 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  // constructor() { //it is a method that will run before the StorePicker Component is created
+  // super();
+  // // console.log('Gonna create a component!');
+  // this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  goToStore = event => {
+    /*if you need to access "this" inside of a custom method you must use this 
+    syntax or use the contructor to bind*/
+    // console.log("Going to store");
+
+    // 1. Stop the form from submitting
+    event.preventDefault();
+
+    // 2. Get the text from that input
+    // const storeName = document.querySelector ===> Golden Rule in React: do not touch the DOM
+    const storeName = this.myInput.current.value;
+
+    // 3. Change the page to /store/wahtever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+    /* StorePicker is a child of the Router, so we a ctually have access to the 
+    methods of the Router */
+  };
+
+  handleClick() {
+    alert("Heyyyyy!");
+  }
+
+  //   componentDidMount() {
+  //     console.log("MOUNTED!!!");
+  //   }
+
   render() {
-      console.log(this)
+    // console.log(this);
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         {/* comments (there is no regular html or doubleslash comments, just regular block comments) */}
         <h2>Please Enter A Store</h2>
+
+        {/* <button onClick={this.handleClick}>CLick me!</button> */}
+
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
@@ -21,3 +59,7 @@ class StorePicker extends React.Component {
 }
 
 export default StorePicker;
+
+
+/*state is essencially just an object, an object that lives inside of a component that 
+stores all of the data that that component and maybe some of its children may need*/
