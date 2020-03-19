6e68feaee3bd6369ff4357155da90e750254dae8
Andressa Tonietto <andressatonietto@MacBook-Air-de-Andressa.local>
2020-03-18 22:10:05 -0300

    commit teste

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,3 +1,5 @@
+// hey teacher
+
 import React from "react"; //imports the entire React package
 import { render } from "react-dom"; //imports just one method from the package
 import Router from "./components/Router";
